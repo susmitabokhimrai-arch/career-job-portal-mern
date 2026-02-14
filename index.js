1de42db7894f0ae4c5b3b8bd490efe56aa3441bd
Kajal sharma <shrmakajal963@gmail.com>
2026-02-14 17:16:26 +0545

    database connection made

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config({});
 
 const app = express();
 
@@ -15,10 +17,7 @@ const corsOptions ={
 
 app.use(cors(corsOptions));
 
-
-const PORT=3000;
-
-
+const PORT=process.env.PORT || 3000;
 
 app.listen(PORT,()=>{
 console.log('Server running at port ${PORT}');
