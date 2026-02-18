7ba4daf1e405f7b48c4fc0d1893dc490bb213dad
Kajal sharma <shrmakajal963@gmail.com>
2026-02-18 19:40:39 +0545

    created models Application model with job, applicant, and status

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,12 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
 dotenv.config({});
 
 const app = express();
 
- 
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
@@ -20,6 +21,7 @@ app.use(cors(corsOptions));
 const PORT=process.env.PORT || 3000;
 
 app.listen(PORT,()=>{
+    connectDB();
 console.log('Server running at port ${PORT}');
 })
 
