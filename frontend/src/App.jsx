a7b03d5b01e72afb5d5b163cccf5ef503d9d55da
Susmita Rai <suci@Susmitas-MacBook-Air.local>
2026-03-11 13:50:34 +0545

    filter jobs 11:05:45

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,8 @@ import JobDescription from './components/JobDescription'
 import Companies from './components/admin/Companies'
 import CompanyCreate from './components/admin/CompanyCreate'
 import CompanySetup from './components/admin/CompanySetup'
+import AdminJobs from "./components/admin/AdminJobs";
+
 const appRouter = createBrowserRouter([
   {
     path:'/',
@@ -52,8 +54,12 @@ const appRouter = createBrowserRouter([
     path:"/admin/companies/:id",
     element:<CompanySetup/>
   },
-]
-)
+  {
+    path:"/admin/jobs",
+    element:<AdminJobs/>
+  },
+
+])
 
 function App() {
   
