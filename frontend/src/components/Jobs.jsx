02c5e3d2f96da888c5b90e0675a0fbf4136a6f24
Susmita Rai <suci@Susmitas-MacBook-Air.local>
2026-03-14 13:19:25 +0545

    protected route

--- frontend/src/components/Jobs.jsx
+++ frontend/src/components/Jobs.jsx
@@ -3,30 +3,31 @@ import Navbar from "./shared/Navbar";
 import FilterCard from "./FilterCard";
 import Job from "./Job";
 import { useSelector } from "react-redux";
+import { motion } from "framer-motion";
 
 //const jobsarray = [1, 2, 3, 4, 5, 6, 7, 8];
 
 const Jobs = () => {
-  const {allJobs, searchedQuery} = useSelector(store=>store.job);
+  const { allJobs, searchedQuery } = useSelector(store => store.job);
   const [filterJobs, setFilterJobs] = useState(allJobs);
 
-useEffect(()=>{
-if(searchedQuery){
-const filteredJobs = allJobs.filter((job)=>{
-  return job?.title?.toLowerCase().includes(searchedQuery.toLowerCase()) ||
-  job?.description?.toLowerCase().includes(searchedQuery.toLowerCase()) ||
-  job?.location?.toLowerCase().includes(searchedQuery.toLowerCase())
-})
-setFilterJobs(filteredJobs)
-  }else{
-    setFilterJobs(allJobs)
-  }
-},[allJobs, searchedQuery]);
+  useEffect(() => {
+    if (searchedQuery) {
+      const filteredJobs = allJobs.filter((job) => {
+        return job?.title?.toLowerCase().includes(searchedQuery.toLowerCase()) ||
+          job?.description?.toLowerCase().includes(searchedQuery.toLowerCase()) ||
+          job?.location?.toLowerCase().includes(searchedQuery.toLowerCase())
+      })
+      setFilterJobs(filteredJobs)
+    } else {
+      setFilterJobs(allJobs)
+    }
+  }, [allJobs, searchedQuery]);
 
   return (
     <div>
       <Navbar />
-<div className="max-w-7xl mx-auto mt-5">
+      <div className="max-w-7xl mx-auto mt-5">
         <div className="flex gap-5">
 
           {/* Sidebar */}
@@ -35,21 +36,26 @@ setFilterJobs(filteredJobs)
           </div>
 
           {/* Jobs Section */}
-            {
+          {
             filterJobs.length <= 0 ? <span>Job not found</span> : (
               <div className="flex-1 h-[88vh] overflow-y-auto pb-5">
-               <div className="grid grid-cols-3 gap-4">
-                {
-                filterJobs.map((job) => (
-                  <div key={job?._id}>
-                    <Job job={job} />
-                    </div>
-                ))
-                }
+                <div className="grid grid-cols-3 gap-4">
+                  {
+                    filterJobs.map((job) => (
+                      <motion.div
+                        initial={{ opacity: 0, x: 100 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        exit={{ opacity: 0, x: -100 }}
+                        transition={{ duration: 0.3 }}
+                        key={job?._id}>
+                        <Job job={job} />
+                      </motion.div>
+                    ))
+                  }
+                </div>
               </div>
-          </div>
-              )
-            }
+            )
+          }
         </div>
       </div>
 
