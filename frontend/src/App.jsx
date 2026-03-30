0402e770de0fc5366c68622ecdfe6f38c6090507
nikki <nikitarajrauniyar@gmail.com>
2026-03-30 09:36:07 +0545

    added saved job look

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import AdminJobs from "./components/admin/AdminJobs";
 import PostJob from './components/admin/PostJob'
 import Applicants from './components/admin/Applicants'
 import ProtectedRoute from './components/admin/ProtectedRoute'
+import SavedJobs from './components/SavedJobs'
 
 const appRouter = createBrowserRouter([
   {
@@ -43,6 +44,10 @@ const appRouter = createBrowserRouter([
   {
     path:'/profile',
     element:<Profile/>
+  },
+  {
+    path: '/saved-jobs',       // added
+    element: <SavedJobs />     // added
   },
   //admin part here start
   {
