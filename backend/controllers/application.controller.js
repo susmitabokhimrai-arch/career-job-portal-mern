8a546a79f22278b4614df2cc62446c7aef5197b3
Susmita Rai <suci@Susmitas-MacBook-Air.local>
2026-04-01 13:34:22 +0545

    backend suc

--- backend/controllers/application.controller.js
+++ backend/controllers/application.controller.js
@@ -1,129 +1,138 @@
-import { Application } from "../models/application.model.js";
-import { Job } from "../models/job.model.js";
+ import { Application} from "../models/application.model.js";
+ import { Job } from "../models/job.model.js";
 
-//apply for internship
-export const applyJob = async (req, res) => {
-    try {
+// apply for internship
+
+ export const applyJob = async (req,res) => {
+    try{
         const userId = req.id;
         const jobId = req.params.id;
-        if (!jobId) {
+        if(!jobId){
             return res.status(400).json({
-                message: "Internship id is required.",
-                success: false
-            })
-        };
-        //check if the user has already applied for job
-        const existingApplication = await Application.findOne({ job: jobId, applicant: userId });
+                message:"Internship id is required.",
+                success:false
+            });
+        }
+        //check for duplicate application
 
-        if (existingApplication) {
+        const existingApplication = await Application.findOne({ job: jobId, applicant: userId});
+        
+        if(existingApplication){
             return res.status(400).json({
-                message: "You have already applied.",
-                success: false
+                message:"You have already applied.",
+                success:false
             });
         }
         // check if the job exists
         const job = await Job.findById(jobId);
-        if (!job) {
+        if(!job){
             return res.status(404).json({
-                message: "Internship not found.",
+                message:"Internship not found.",
                 success: false
             });
         }
         // create a new application
         const newApplication = await Application.create({
-            job: jobId,
-            applicant: userId,
+            job:jobId,
+            applicant:userId,
         });
 
         job.applications.push(newApplication._id);
         await job.save();
 
         return res.status(201).json({
-            message: "Internship applied successfully.",
-            success: true
+            message:"Internship applied successfully.",
+            success:true
         });
+
     } catch (error) {
         console.log(error);
-        // return error response
-        return res.status(500).json({
-            message: error.message || "Internal server error",
-            success: false
-        });
     }
 };
+
 // get student applied internships
-export const getAppliedJobs = async (req, res) => {
-    try {
-        const userId = req.id;
-        const application = await Application.find({ applicant: userId }).sort({ createdAt: -1 }).populate({
-            path: 'job',
-            populate: {
-                path: 'company',
-            }
-        });
+export const getAppliedJobs = async (req,res) => {
+    try{
+const userId = req.id;
 
-        if (!application || application.length === 0) {
-            return res.status(404).json({
-                message: "No Applications found.",
-                success: false
-            });
-        }
-        return res.status(200).json({
-            application,
-            success: true
-        });
+const application = await Application.find({applicant:userId}).sort({createdAt:-1}).populate({
+    path:'job',
+    //options:{sort:{createdAt:-1}},
+    populate:{
+        path:'company',
+         //options:{sort:{createdAt:-1}},
+    }
+});
+
+if(!application || application.length ===0){
+return res.status(404).json({
+    message:"No Applications found.",
+    success:false
+});
+}
+
+return res.status(200).json({
+    application,
+    success:true
+})
     } catch (error) {
         console.log(error);
-        // return error response
-        return res.status(500).json({
-            message: error.message || "Internal server error",
-            success: false
-        });
     }
 };
-//admin can check total no. of applied jobs by user
 
-export const getApplicants = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const job = await Job.findById(id).populate({
-            path: 'applications',
-            options: { sort: { createdAt: -1 } },
-            populate: {
-                path: 'applicant'
-            }
-        });
-        if (!job) {
-            return res.status(404).json({
-                message: 'Internship not found.',
-                success: false
-            })
-        };
-        return res.status(200).json({
-            job,
-            success: true
-        });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            message: error.message || "Internal server error",
-            success: false
-        });
+//get all applicants of applied jobs by user
+
+export const getApplicants = async (req,res) =>
+{
+    try{
+        const {id} = req.params;
+
+const job = await Job.findById(id).populate({
+    path:'applications',
+    options: {sort:{createdAt:-1}},
+    populate:{
+        path:'applicant'
     }
-};
+});
+
+if(!job){
+    return res.status(404).json({
+        message:'Internship not found.',
+        success:false
+    });
+}
+
+return res.status(200).json({
+    job,
+    success:true
+});
+    } catch(error) {
+        console.log(error);
+    }
+}
+
 // update application status ATS logic
-export const updateStatus = async (req, res) => {
-    try {
-        const { status } = req.body;
-        const applicationId = req.params.id;
-        const allowedStatus = [
+export const updateStatus = async (req,res) => {
+    try{
+        const {status} = req.body;
+  const applicationId = req.params.id;
+
+//if(!status) {
+    // return res.status(400).json({
+       // message:'Status is required.',
+       // success:false
+    //})
+//};
+
+const allowedStatus = [
             'applied',
             'shortlisted',
             'interview',
             'selected',
             'rejected'
         ];
-        //  Validate status
+
+
         if (!status || !allowedStatus.includes(status.toLowerCase())) {
             return res.status(400).json({
                 message: 'Invalid status value.',
@@ -131,28 +140,26 @@ export const updateStatus = async (req, res) => {
             });
         }
 
-        // find application by application id
-        const application = await Application.findOne({ _id: applicationId });
-        if (!application) {
-            return res.status(404).json({
-                message: 'Application not found.',
-                success: false
-            });
-        };
 
-        // update status
-        application.status = status.toLowerCase();
-        await application.save();
-        return res.status(200).json({
-            message: 'Application status updated successfully.',
-            success: true
-        });
+// find application by application id
+const application = await Application.findOne({_id:applicationId});
+if(!application) {
+    return res.status(404).json({
+        message:'Application not found.',
+        success:false
+    });
+}
 
+// update status
+application.status = status.toLowerCase();
+await application.save();
+
+return res.status(200).json({
+        message:'Aplication status updated successfully.',
+        success:true
+    });
+    
     } catch (error) {
         console.log(error);
-        return res.status(500).json({
-            message: error.message || 'Internal server error',
-            success: false
-        });
     }
 };
