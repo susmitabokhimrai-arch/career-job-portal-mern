e5c29048ae1c77ca7171d3004537f13be79f8734
Susmita Rai <suci@Susmitas-MacBook-Air.local>
2026-04-01 12:32:08 +0545

    update by suc

--- backend/routes/company.routes.js
+++ backend/routes/company.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import isAuthenticated from "../middlewares/isAuthenticate.js";
 import { getCompany,  registerCompany,  getCompanyById,  updateCompany } from "../controllers/company.controller.js";
+import { singleUpload } from "../middlewares/multer.js";
 
 const router = express.Router();
 
